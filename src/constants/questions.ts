63db48f2081b54b2af5ce488a43ff7a949ca11da
MatheusMoreiraDosSantos <matheusmoreira.santos699@gmail.com>
2024-11-01 12:48:12 -0300

    Cria as perguntas fixas no codigo

---
+++ src/constants/questions.ts
@@ -0,0 +1,92 @@
+export const multipleChoiceQuestions = [
+  {
+    question:
+      "A pessoa tem demonstrado mudanças de comportamento recentemente?",
+    options: [
+      "Sim, mudanças significativas",
+      "Sim, algumas mudanças",
+      "Não, o comportamento é o mesmo",
+      "Não sei dizer",
+    ],
+  },
+  {
+    question:
+      "Você notou que a pessoa está mais isolada ou evitando contato social?",
+    options: [
+      "Sim, está muito mais isolada",
+      "Sim, um pouco mais isolada",
+      "Não, está interagindo normalmente",
+      "Não sei dizer",
+    ],
+  },
+  {
+    question:
+      "A pessoa tem falado sobre sentimentos de desesperança ou de não ter propósito na vida?",
+    options: [
+      "Sim, com frequência",
+      "Sim, às vezes",
+      "Não, nunca",
+      "Não sei dizer",
+    ],
+  },
+  {
+    question:
+      "Você já ouviu a pessoa mencionar pensamentos sobre morte ou suicídio?",
+    options: [
+      "Sim, várias vezes",
+      "Sim, uma ou duas vezes",
+      "Não, nunca",
+      "Não sei dizer",
+    ],
+  },
+  {
+    question:
+      "A pessoa tem demonstrado comportamentos autodestrutivos, como abuso de álcool ou drogas?",
+    options: [
+      "Sim, com frequência",
+      "Sim, ocasionalmente",
+      "Não, não percebi isso",
+      "Não sei dizer",
+    ],
+  },
+  {
+    question:
+      "A pessoa já fez comentários do tipo “ninguém se importaria se eu sumisse” ou “eu sou um peso para os outros”?",
+    options: [
+      "Sim, frequentemente",
+      "Sim, algumas vezes",
+      "Não, nunca ouvi",
+      "Não sei dizer",
+    ],
+  },
+  {
+    question:
+      "Você percebeu mudanças no sono ou apetite da pessoa (dormindo muito ou pouco, comendo mais ou menos que o normal)?",
+    options: [
+      "Sim, grandes mudanças",
+      "Sim, pequenas mudanças",
+      "Não, está normal",
+      "Não sei dizer",
+    ],
+  },
+  {
+    question:
+      "A pessoa está mais irritada ou demonstra mudanças bruscas de humor?",
+    options: [
+      "Sim, frequentemente",
+      "Sim, ocasionalmente",
+      "Não, o humor está normal",
+      "Não sei dizer",
+    ],
+  },
+  {
+    question:
+      "Você acredita que a pessoa possa estar passando por uma fase muito difícil ou sofrendo emocionalmente?",
+    options: [
+      "Sim, claramente",
+      "Sim, mas não tenho certeza",
+      "Não, não parece",
+      "Não sei dizer",
+    ],
+  },
+];
